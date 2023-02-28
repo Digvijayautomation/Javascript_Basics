49cfcd36bdf8eb850ecd20322ab7e4a51e1fe530
Digvijayautomation <dtikka6@gmail.com>
2023-02-28 17:58:06 +0530

    Learning Javascript

---
+++ basics1.js
@@ -0,0 +1,36 @@
+//Print statement in javascript
+console.log("Learning Javascript")
+
+
+
+// Varibales in js can be definded by using var,let, const
+// We can redecleare and reassign varibale the with var
+// We can not redecleare but reassign varibale the with let
+// We can not redecleare nor reassign varibale the with const
+// from version es6 let and const are introduced
+// while defining no need to explicity use the data type
+
+//datatypes= number, string, boolean, null, undefined
+
+
+//Varibale 
+var a=23  // it will automatically understand that this is number
+var b="digvjay" // it will automatically understand that this is string
+console.log(a,b)
+console.log(typeof(a))  // to find the type of varibale
+
+let c=7
+let d="tikka"
+console.log(c,d)
+console.log(typeof(d))  // to find the type of varibale
+
+const x=7
+const y="cybage"
+console.log(x,y)
+console.log(typeof(x))  // to find the type of varibale
+
+
+let is_salaray=true //boolean data type
+
+
+
